a5ac7cf0950c4272a1b113dfa6ed246f301723c6
BiuBiuBia <46010765+BiuBiuBia@users.noreply.github.com>
2020-07-15 10:42:52 +0800

    SetPassWord+GetAllOrder

--- src/servlet/order/FinishOrder.java
+++ src/servlet/order/FinishOrder.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FinishOrder
  */
-@WebServlet("/FinishOrder")
+@WebServlet("/api/ordermanage/finishOrder")
 public class FinishOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
