534f82094a926fc2cc8905707d843e30c3de3594
18gy888 <49858931+18gy888@users.noreply.github.com>
2020-07-17 16:48:55 +0800

    FinishOrder接口与GetOrderListByUserId.接口更新
    FinishOrder接口与GetOrderListByUserId.接口更新

--- src/servlet/order/FinishOrder.java
+++ src/servlet/order/FinishOrder.java
@@ -21,7 +21,7 @@ import net.sf.json.JSONArray;
 /**
  * Servlet implementation class FinishOrder
  */
-@WebServlet("/FinishOrder")
+@WebServlet("/api/ordermanage/finishOrder")
 public class FinishOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -49,64 +49,59 @@ public class FinishOrder extends HttpServlet {
 		response.setContentType("text/json; charset=utf-8");
 		PrintWriter out=response.getWriter();
 		Connection conn=null;
+		Statement stmt = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn=DriverManager.getConnection("jdbc:mysql://106.13.201.225:3306/coffee?useSSL=false&serverTimezone=GMT","coffee","TklRpGi1");
-			Statement stmt=conn.createStatement();
+			stmt=conn.createStatement();
 			ServletInputStream is;
 			try {
-				is=request.getInputStream();
-				int nRead=1;
-				int nTotalRead=0;
-				byte[] bytes=new byte[10240];
-				while(nRead>0) {
-					nRead=is.read(bytes,nTotalRead,bytes.length-nTotalRead);
-					if(nRead>0)
-						nTotalRead+=nRead;
+				is = request.getInputStream();
+				int nRead = 1;
+				int nTotalRead = 0;
+				byte[] bytes = new byte[10240];
+				while (nRead > 0) {
+					nRead = is.read(bytes, nTotalRead, bytes.length - nTotalRead);
+					if (nRead > 0)
+						nTotalRead = nTotalRead + nRead;
 				}
-				String str=new String(bytes,0,nTotalRead,"utf-8");
-				JSONObject jsonObj=JSONObject.fromObject(str);
-				String orderId=jsonObj.getString("orderId");
-				String sql="select * from orders where orderId= ?";
-				String sql2="select * from meal_order where orderId= ?";
+				String str = new String(bytes, 0, nTotalRead, "utf-8");
+				JSONObject jsonObj = JSONObject.fromObject(str);
+				/*以创建订单时返回的orderId为传入参数*/
+				String orderId = jsonObj.getString("orderId");
+				String sql="update orders SET status ='已完成' where orderId= ?";
 				PreparedStatement ps=conn.prepareStatement(sql);
-				PreparedStatement ps2=conn.prepareStatement(sql2);
-				ps.setString(1,orderId);
-				ps2.setString(1, orderId);
-				ResultSet rs=ps.executeQuery();
-				ResultSet rs2=ps2.executeQuery();
+				ps.setString(1, orderId);
+				ps.executeUpdate();
 				JSONObject jsonobj=new JSONObject();
-				JSONObject jsonobj2=new JSONObject();
-				JSONArray jsonarray = new JSONArray();
-				rs.next();
-				jsonobj.put("orderId",rs.getString("orderId"));
-				jsonobj.put("userId",rs.getString("userId"));
-				jsonobj.put("createdTime",rs.getString("createdTime"));
-				rs.close();
-				while(rs2.next()) {
-					jsonobj2.put("mealId",rs2.getString("mealId"));
-					jsonobj2.put("amount",rs2.getString("amount"));
-					jsonarray.add(jsonobj2);
-				}
-				if(jsonobj2.isEmpty()) {
-					jsonobj.put("success",false);
-					jsonobj.put("msg","��ȡʧ��");
-				}else {
-					jsonobj.put("success",true);
-					jsonobj.put("msg","��ȡ�ɹ�");
-				}
-				jsonobj.put("data",jsonarray);
+				jsonobj.put("success", true);
+				jsonobj.put("msg","订单状态修改成功");
 				out=response.getWriter();
 				out.println(jsonobj);
-				rs.close();
-				stmt.close();
-				conn.close();
-			}catch(IOException e) {
+				} catch (IOException e) {
 				e.printStackTrace();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+				JSONObject responseJson = new JSONObject();
+				responseJson.put("success",false);
+				responseJson.put("msg", e.getMessage());
+				out.println(responseJson);
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}catch(ClassNotFoundException e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					stmt.close();
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
-		}catch(SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
 		}
-	}
 
 }
